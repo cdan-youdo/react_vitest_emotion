825ef5609228595c7b9a2380c184451d1bf1f636
Dan Corneanu <dan.corneanu@youdo.co.nz>
2024-08-06 10:29:51 +1200

    Remove unnecessary dependencies

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -1,16 +1,10 @@
-import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { ThemeProvider } from '@mui/material/styles';
 import { theme } from 'styles/theme'
 import { StyledDiv } from 'components';
 
 export const AppContainer = () =>
-  <LocalizationProvider dateAdapter={AdapterMoment}>
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <StyledDiv/>
-      </ThemeProvider>
-    </StyledEngineProvider>
-  </LocalizationProvider>
+  <ThemeProvider theme={theme}>
+    <StyledDiv/>
+  </ThemeProvider>
 
 export default AppContainer
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react-swc'
-import browserslistToEsbuild from 'browserslist-to-esbuild'
 
 // https://vitejs.dev/config/
 export default defineConfig(vite => ({
@@ -10,18 +9,6 @@ export default defineConfig(vite => ({
     tsconfigPaths(),
     react({ jsxImportSource: '@emotion/react' })
   ],
-  build: {
-    // Read the target browser configuration from browserslist and pass the
-    // correct browsers to esbuild's target option. See https://github.com/marcofugaro/browserslist-to-esbuild
-    target: browserslistToEsbuild(),
-  },
-  esbuild: {
-    // ESBuild allows you to specify a list of “supported” features that will
-    // be prioritized over the compilation target coming from `build.target`.
-    // See https://esbuild.github.io/api/#supported
-    supported: {
-    },
-  },
   define: {
     // This is a workaround so we don't have to get ENV variables from a different location to create-react-app
     // If we commit to Vite long term we should switch to import.meta.env instead of process.env and prefix our variables with VITE_ instead of REACT_APP_
